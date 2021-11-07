220af75d32ec7b6a2d5a91231cbb53b6592292ad
Adam Freeman <adamgf@gmail.com>
2021-11-07 15:04:16 -0800

    integrating axios for API get to list projects in console log

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { OAuth } from 'oauthio-web';
 
-var username = 'dummy'
+const axios = require('axios');
+
+var username = ''
 
 OAuth.initialize('_kPudQPY3u7LLbJwnCHnB8v8y5M')
 OAuth.popup('basecamp').done(function(result) {
@@ -10,7 +12,20 @@ OAuth.popup('basecamp').done(function(result) {
     // do some stuff with result
     result.me().done(function(data) {
       // do something with `data`, e.g. print data.name
-      alert('Hello ' + data.name + '!') 
+      alert('Hello ' + data.name + '! ' + data.id + ' ' + result.access_token) 
+      console.log('id:' + data.id)
+      console.log('token:' + result.access_token)
+      const config = {
+        method: 'get',
+        url: `https://3.basecampapi.com/${data.id}/people.json`,
+        headers: { Authorization: `Bearer ${result.access_token}`, 'User-Agent' : 'SBDProjectManager (adamgf@gmail.com)', 'Content-Type' : 'application/json' }
+      };
+      const someFunc = async() => {
+          alert('Entered someFunc')
+          let res = await axios(config)
+          alert("All People: " + res);
+      }
+      someFunc()
     })
 })
 
