00ee041327a5acc875349271a866ebaebafb4ab3
Adam Freeman <adamgf@gmail.com>
2021-11-07 22:17:12 -0800

    fetching projects and people using basecamp v3 api

--- src/App.js
+++ src/App.js
@@ -1,36 +1,78 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import { OAuth } from 'oauthio-web';
 
-const axios = require('axios');
 
-var username = ''
+class App extends React.Component {
 
-OAuth.initialize('_kPudQPY3u7LLbJwnCHnB8v8y5M')
-OAuth.popup('basecamp').done(function(result) {
-    console.log(result)
-    // do some stuff with result
-    result.me().done(function(data) {
-      // do something with `data`, e.g. print data.name
-      alert('Hello ' + data.name + '! ' + data.id + ' ' + result.access_token) 
-      console.log('id:' + data.id)
-      console.log('token:' + result.access_token)
+  constructor(props) {
+    super(props)
+    this.state = {
+      people : "",
+      projects : ""    
+    }
+  }
+
+  componentDidMount = async() => {
+
+    const updatePeopleState = (peopleList) => {
+      alert('people: ' + peopleList)
+      this.setState({ people : peopleList })
+    }
+    
+    const updateProjectState = (projectsList) => {
+      alert('projects: ' + projectsList)
+      this.setState({ projects : projectsList})
+    }
+
+    OAuth.initialize('_kPudQPY3u7LLbJwnCHnB8v8y5M')
+    OAuth.popup('basecamp').done(function(result) {
+      console.log(JSON.stringify(result))
+      // do some stuff with result
+
+
+      result.me().done(function(data) {
+        // do something with `data`, e.g. print data.name
+        var userId = data.id 
+        let accounts = data.raw.accounts
+        for (var i=0;i < accounts.length;i++) {
+          if (accounts[i].name === "Stanley Black & Decker") {
+            userId = accounts[i].id
+            break
+          }
+        }
+
+        result.get(`https://3.basecampapi.com/${userId}/people.json`).done(function(peoplelist) {
+          console.log('People list is: ' + JSON.stringify(peoplelist))
+          updatePeopleState(JSON.stringify(peoplelist))
+        })
+
+        result.get(`https://3.basecampapi.com/${userId}/projects.json`).done(function(projectslist) {
+          console.log('Project list is: ' + JSON.stringify(projectslist))
+          updateProjectState(JSON.stringify(projectslist))
+        })
+    
+     
+/*   
       const config = {
         method: 'get',
-        url: `https://3.basecampapi.com/${data.id}/people.json`,
-        headers: { 'Authorization': `Bearer ${result.access_token}`, 'Access-Control-Allow-Origin' : 'https://adamgf.github.io', 'User-Agent' : 'SBDProjectManager (adamgf@gmail.com)', 'Content-Type' : 'application/json' }
-      };
+        withCredentials: true,
+        url: `https://3.basecampapi.com/${userId}/people.json`,
+        headers: { 'Authorization' : `Bearer ${result.access_token}`, 'Content-Type' : 'application/json' }
+      }
       const someFunc = async() => {
-          alert('Entered someFunc')
-          let res = await axios(config)
-          alert("All People: " + res);
+          let res = await OAuth.get(`https://3.basecampapi.com/${userId}/people.json`, config) 
+          alert("All People: " + JSON.stringify(res));
       }
       someFunc()
+ */
+      })
     })
-})
+  }
 
-function App() {
-  return (
+  render() { 
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -43,11 +85,18 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React {username}!!
+          Learn React
         </a>
+        <div>
+          All People: {this.state.people}
+        </div>
+        <div>
+          All Projects: {this.state.projects}
+        </div>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
