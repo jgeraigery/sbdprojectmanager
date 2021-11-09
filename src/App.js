b7489479ccd81b2a52ecec2652b2a2aa5f9ef128
Adam Freeman <adamgf@gmail.com>
2021-11-08 23:10:10 -0800

    person selection part of sbd project manager seems to be working okay

--- src/App.js
+++ src/App.js
@@ -2,27 +2,57 @@ import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import { OAuth } from 'oauthio-web';
+import Gallery from 'react-grid-gallery';
 
+var targetPeopleIDs = []
+
+function onSelectThumbnail() {
+  if (!this.props.item.isSelected) {
+    const personIDToAdd = this.props.item.tags[0].value
+    targetPeopleIDs.push(personIDToAdd)
+    alert(this.props.item.caption + ' has been added!')
+    this.props.item.isSelected = true
+  }
+  else {
+    const personIDToBeRemoved = this.props.item.tags[0].value
+    const index = targetPeopleIDs.indexOf(personIDToBeRemoved)
+    if (index !== -1) {
+      targetPeopleIDs.splice(index, 1); 
+    }
+    alert(this.props.item.caption + ' has been removed!')
+    this.props.item.isSelected = false
+  }
+  alert('Current IDs are: ' + JSON.stringify(targetPeopleIDs))
+}
 
 class App extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      people : "",
+      people : [],
+      peoplethumbnails : [],
       projects : ""    
     }
   }
 
   componentDidMount = async() => {
 
-    const updatePeopleState = (peopleList) => {
-      alert('people: ' + peopleList)
-      this.setState({ people : peopleList })
+    const updatePeopleState = (peopleList, peopleThumbnails) => {
+      //alert('people: ' + peopleList)
+      var existingpeople = this.state.people
+      var existingthumbnails = this.state.peoplethumbnails
+      if (peopleList && peopleList.length > 0) {
+        
+        const newpeople = existingpeople.concat(peopleList) 
+        const newimagethumbnails = existingthumbnails.concat(peopleThumbnails)
+        alert('Total people count: ' + newpeople.length)
+        this.setState({ people : newpeople, peoplethumbnails : newimagethumbnails })
+      }
     }
     
     const updateProjectState = (projectsList) => {
-      alert('projects: ' + projectsList)
+      //alert('projects: ' + projectsList)
       this.setState({ projects : projectsList})
     }
 
@@ -43,10 +73,76 @@ class App extends React.Component {
           }
         }
 
-        result.get(`https://3.basecampapi.com/${userId}/people.json`).done(function(peoplelist) {
-          console.log('People list is: ' + JSON.stringify(peoplelist))
-          updatePeopleState(JSON.stringify(peoplelist))
-        })
+        const getFunc = async(theResult, peoplepage) => {
+          theResult.get(`https://3.basecampapi.com/${userId}/people.json?page=${peoplepage}`)
+          .done(function(peoplelist) {
+            var ids = []
+            var thumbnailImages = []
+            for (const person of peoplelist) {
+              ids.push(person.id)
+              const representationalImage = {
+                src: person.avatar_url,
+                thumbnail: person.avatar_url,
+                thumbnailWidth: 64,
+                thumbnailHeight: 64,
+                isSelected: false,
+                caption: person.name,
+                tags: [{ value: person.id, title: person.name }]
+              }
+              thumbnailImages.push(representationalImage)
+            }
+            if (ids.length === 0) {
+              alert('Got zero length of people for page ' + peoplepage + '!!')
+            }
+            else {
+              updatePeopleState(ids, thumbnailImages)
+              getFunc(result, peoplepage + 1)
+            }
+          })
+          .fail(function(err) {
+            alert('Failure condition was triggered ' + err) 
+          })            
+        }
+
+        getFunc(result, 1) 
+
+       /*
+          result.get(`https://3.basecampapi.com/${userId}/people.json?page=2`)
+          .done(function(peoplelist) {
+            console.log('People list is: ' + JSON.stringify(peoplelist))
+            var ids = [] 
+            for (const person of peoplelist) {
+              ids.push(person.id)
+            }
+            updatePeopleState(ids)
+          })
+          .fail(function (err) {
+          });
+
+          result.get(`https://3.basecampapi.com/${userId}/people.json?page=3`)
+          .done(function(peoplelist) {
+            console.log('People list is: ' + JSON.stringify(peoplelist))
+            var ids = [] 
+            for (const person of peoplelist) {
+              ids.push(person.id)
+            }
+            updatePeopleState(ids)
+          })
+          .fail(function (err) {
+          });
+
+          result.get(`https://3.basecampapi.com/${userId}/people.json?page=4`)
+          .done(function(peoplelist) {
+            console.log('People list is: ' + JSON.stringify(peoplelist))
+            var ids = [] 
+            for (const person of peoplelist) {
+              ids.push(person.id)
+            }
+            updatePeopleState(ids)
+          })
+          .fail(function (err) {
+          });
+        */
 
         result.get(`https://3.basecampapi.com/${userId}/projects.json`).done(function(projectslist) {
           console.log('Project list is: ' + JSON.stringify(projectslist))
@@ -69,29 +165,22 @@ class App extends React.Component {
  */
       })
     })
+
+    
   }
 
   render() { 
     return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <div>
-          All People: {this.state.people}
+          All People:
+          <div>
+            <Gallery images={this.state.peoplethumbnails} onClickThumbnail={onSelectThumbnail} />
+          </div>     
         </div>
         <div>
-          All Projects: {this.state.projects}
+          All Projects: 
         </div>
       </header>
     </div>
